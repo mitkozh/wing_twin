d0347ed024578442d002205e032c43b921a549f4
m.german <m.german@student.tue.nl>
2026-05-28 16:39:53 +0200

    Fixed the bug of declaration

--- scripts/analysis/__init__.py
+++ scripts/analysis/__init__.py
@@ -3,6 +3,20 @@ Analysis module - recording and data loading utilities.
 """
 
 from .loader import DataLoader, RecordingData
-from .recorder import DataRecorder, save_fatigue_state, load_fatigue_state
+from .recorder import (
+    DataRecorder,
+    save_fatigue_state,
+    load_fatigue_state,
+    save_life_prediction_state,
+    load_life_prediction_state,
+)
 
-__all__ = ["DataLoader", "RecordingData", "DataRecorder", "save_fatigue_state", "load_fatigue_state"]
+__all__ = [
+    "DataLoader",
+    "RecordingData",
+    "DataRecorder",
+    "save_fatigue_state",
+    "load_fatigue_state",
+    "save_life_prediction_state",
+    "load_life_prediction_state",
+]
