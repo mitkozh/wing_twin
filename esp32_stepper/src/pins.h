aa5fd1ea288c400d8734b8574efd8514f0b1672c
Dimitrios Economidis <d.economidis@student.tue.nl>
2026-06-15 17:07:11 +0200

    changed pin numbers to test esp_stepper

--- esp32_stepper/src/pins.h
+++ esp32_stepper/src/pins.h
@@ -1,8 +1,8 @@
 #ifndef PINS_H
 #define PINS_H
 
-#define STEP_PIN      13
-#define DIR_PIN       21
-#define ENABLE_PIN    23
+#define STEP_PIN      2
+#define DIR_PIN       5
+#define ENABLE_PIN    19
 
 #endif
